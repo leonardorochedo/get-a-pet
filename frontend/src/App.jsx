22af74090d1941b0098ba7ab654a7072e9a5542f
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-11-11 13:49:28 -0300

    create navbar and footer structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,10 @@ import {
   Route,
 } from "react-router-dom";
 
+// Components
+import { Navbar } from "./components/layout/Navbar";
+import { Footer } from "./components/layout/Footer";
+
 // Pages
 import { Home } from "./components/pages/Home";
 import { Login } from "./components/pages/Auth/Login";
@@ -13,11 +17,13 @@ export function App() {
 
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer>
+            <p><span className="bold">Get A Pet</span> &copy; 2021</p>
+        </footer>
+    );
+}
