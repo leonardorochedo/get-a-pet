5d7bc416945ec86797d7b74040ea2542607344fe
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-11-11 09:05:30 -0300

    route create a pet finish

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -1,7 +1,74 @@
 const Pet = require('../models/Pet')
 
+// helpers
+const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
+
 module.exports = class PetController {
     static async create(req, res) {
-        res.json({message: 'Deu certo!'})
+        
+        const {name, age, weight, color} = req.body
+
+        const available = true
+
+        // images
+
+        // validations
+        if(!name) {
+            res.status(422).json({
+                message: 'O nome é obrigatório!'
+            })
+        }
+
+        if(!age) {
+            res.status(422).json({
+                message: 'A idade é obrigatório=a!'
+            })
+        }
+
+        if(!weight) {
+            res.status(422).json({
+                message: 'O peso é obrigatório!'
+            })
+        }
+
+        if(!color) {
+            res.status(422).json({
+                message: 'A cor é obrigatória!'
+            })
+        }
+
+        // get a user owner
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        // create a pet
+        const pet = new Pet({
+            name,
+            age,
+            weight,
+            color,
+            available,
+            images: [],
+            user: { // passando o dono do pet
+                _id: user._id,
+                name: user.name,
+                image: user.image,
+                phone: user.phone
+            }
+        })
+
+        try {
+            const newPet = await pet.save() // salvando no banco
+            res.status(201).json({
+                message: 'Pet cadastrado com sucesso!',
+                newPet, // mandando pro front
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: err
+            })
+        }
+
     }
 }
