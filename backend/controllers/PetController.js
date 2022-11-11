20abfc65f0d571a38a0d1e30a4e38c36d2b03a3c
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-11-11 09:47:52 -0300

    create route and controller to get pet by id

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -3,6 +3,7 @@ const Pet = require('../models/Pet')
 // helpers
 const getToken = require('../helpers/get-token')
 const getUserByToken = require('../helpers/get-user-by-token')
+const ObjectId = require('mongoose').Types.ObjectId
 
 module.exports = class PetController {
     static async create(req, res) {
@@ -122,6 +123,33 @@ module.exports = class PetController {
         // send to frontend
         res.status(200).json({
             pets
+        })
+    }
+
+    static async getPetById(req, res) {
+
+        const id = req.params.id
+
+        // verify ID
+        if(!ObjectId.isValid(id)) {
+            res.status(422).json({
+                message: 'ID inválido'
+            })
+            return
+        }
+
+        // check if pet exists
+        const pet = await Pet.findById(id)
+
+        if(!pet) {
+            res.status(404).json({
+                message: 'O Pet não existe!'
+            })
+            return
+        }
+
+        res.status(200).json({
+            pet
         })
     }
 }
--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -11,5 +11,6 @@ router.post('/create', verifyToken, imageUpload.array('images'), PetController.c
 router.get('/mypets', verifyToken, PetController.getAllUserPets)
 router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions)
 
+router.get('/:id', PetController.getPetById)
 router.get('/', PetController.getAll)
 module.exports = router
