92d5048d9a124f8e92b4f2321d3072c95aae9320
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-11-12 11:51:34 -0300

    form api patch finish, and send image to backend to | and preview image profile finish

--- frontend/src/components/pages/User/Profile.jsx
+++ frontend/src/components/pages/User/Profile.jsx
@@ -7,11 +7,15 @@ import '../../form/Form.css';
 
 import { Input } from '../../form/Input';
 
+import useFlashMessage from '../../../hooks/useFlashMessage';
+
 export function Profile() {
 
     const [user, setUser] = useState({})
+    const [preview, setPreview] = useState()
     // pegando o token via lS
     const [token] = useState(localStorage.getItem('token') || '')
+    const {setFlashMessage} = useFlashMessage()
 
     useEffect(() => {
         // dando um get na api que retorna as informacoes do user via token
@@ -25,21 +29,55 @@ export function Profile() {
 
     }, [])
 
-    function onFileChange() {
-
+    function onFileChange(e) {
+        setPreview(e.target.files[0]) // preview da image
+        setUser({...user, [e.target.name]: e.target.files[0]}) // setando a image no perfil
     }
 
-    function handleChange() {
+    function handleChange(e) {
+        setUser({...user, [e.target.name]: e.target.value})
+    }
 
+    async function handleSubmit(e) {
+        e.preventDefault()
+        
+        let msgType = 'sucess'
+
+        const formData = new FormData()
+
+        const userFormData = await Object.keys(user).forEach((key) => 
+            formData.append(key, user[key])
+        )
+
+        // dando o update
+        const data = await api.patch(`/users/edit/${user._id}`, formData, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+                'Content-Type': 'multipart/form-data' // backend entender que esta indo uma imagem
+            }
+        }).then((response) => {
+            return response.data
+        }).catch((err) => {
+            let msgType = 'sucess'
+            return err.response.data
+        })
+
+        setFlashMessage(data.message, msgType)
     }
 
     return (
-        <section className='form_container'>
+        <section>
             <div className="profile_container">
                 <h1>Profile</h1>
-                <p>Preview Image</p>
+                {(user.image || preview) && (
+                    <img src={
+                        preview
+                        ? URL.createObjectURL(preview)
+                        : `http://localhost:5000//images/users/${user.image}`}
+                        alt={user.name} />
+                )}
             </div>
-            <form>
+            <form onSubmit={handleSubmit} className='form_container'>
                 <Input 
                     text="Imagem"
                     type="file"
