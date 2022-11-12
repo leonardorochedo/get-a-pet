029d1546f7838b3c42d70c7cdb075179588e0a59
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-11-12 10:42:50 -0300

    create login page styles and structure

--- frontend/src/components/pages/Auth/Login.jsx
+++ frontend/src/components/pages/Auth/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import { useState, useContext } from 'react';
+
+import { Input } from '../../form/Input';
+import { Link } from 'react-router-dom';
+
+import '../../form/Form.css';
+
+// CONTEXT
+import { Context } from '../../../context/UserContext';
 
 export function Login() {
+
+    function handleChange() {
+
+    }
+
     return (
-        <section>
+        <section className='form_container'>
             <h1>Login</h1>
+            <form >
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite seu email"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Senha"
+                    type="password"
+                    name="password"
+                    placeholder="Digite sua senha"
+                    handleOnChange={handleChange}
+                />
+                <input type="submit" value="Entrar" />
+            </form>
+            <p>Não tem conta? <Link to="/register">Clique Aqui.</Link></p>
         </section>
     );
 }
