ff93992c65ebc0e24835272bfc0599d5acb7c467
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-11-13 15:21:25 -0300

    create structure and style from home page

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,7 +1,47 @@
+import api from '../../utils/api';
+
+import { useState, useEffect } from 'react';
+
+import './Home.css';
+
+import { Link } from 'react-router-dom';
+
 export function Home() {
+
+    const [pets, setPets] = useState([])
+
+    useEffect(() => {
+        api.get('/pets')
+        .then((response) => {
+            setPets(response.data.pets)
+        })
+    }, [])
+
     return (
         <section>
-            <h1>Home</h1>
+            <div className='pet_home_header'>
+                <h1>Adote um Pet</h1>
+                <p>Veja os detalhes de cada um e conheça o tutor deles</p>
+            </div>
+            <div className='pet_container'>
+                {pets.length > 0 && (
+                    pets.map((pet) => (
+                        <div className='pet_card'>
+                            <div className="pet_card_image" style={{ backgroundImage: `url(http://localhost:5000//images/pets/${pet.images[0]})` }} ></div>
+                            <h3>{pet.name}</h3>
+                            <p><span className="bold">Peso: </span>{pet.weight}</p>
+                            {pet.available ? (
+                                <Link to={`pet/${pet._id}`}>Mais detalhes</Link>
+                            ) : (
+                                <p className='adopted_text'>Adotado</p>
+                            )}
+                        </div>
+                    ))
+                )}
+                {pets.length === 0 && (
+                    <p>Não há pets cadastrados ou disponíveis para a adoção no momento!</p>
+                )}
+            </div>
         </section>
     )
 }
